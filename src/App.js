b55cd9481d15d693fcbad0fe92f66187c1c4aa9b
1neye <illya.landar@gmail.com>
2021-03-29 23:17:56 +0300

    regionsTest

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import './App.css';
 import PostContainer from "./components/Post/PostContainer";
 import {NavLink, Route} from "react-router-dom";
 import Login from "./components/Login/Login";
+import fs from 'fs'
+import xml2js from 'xml2js'
+import regions from './components/regions.xml'
 
 let App = (props) => {
+    // const fs = require('fs')
+    // const xml2js = require('xml2js')
+    const parser = new xml2js.Parser()
+
+    fs.readFile(regions , (err, data) => {
+        parser.parseString(data, (err, result) => {
+            console.log(result)
+        })
+    })
     return <div>
         <NavLink to={'/login'}>Login </NavLink>
         <NavLink to={'/posts'}>Posts</NavLink>
